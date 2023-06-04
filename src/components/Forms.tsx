d3d4342dac9bef74b2b09484952efec21f98b823
Lachicagradiadora <200379@unsaac.edu.pe>
2023-06-03 22:10:34 -0500

    render of form component

---
+++ src/components/Forms.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+
+const Form = () => {
+  const [inputValues, setInputValues] = useState({
+    nick: '',    
+    avatar: '',    
+    subMonths: 0,    
+    description: '',      
+  })
+
+
+  const handleSubmit = () => {}
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setInputValues({
+      ...inputValues,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  return(
+    <div>
+      <form 
+        onSubmit={handleSubmit}
+
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <input onChange={handleChange} value={inputValues.nick} type="text" name="nick" placeholder="nick"/>
+        <input onChange={handleChange} value={inputValues.avatar} type="text" name="avatar" placeholder="avatar"/>
+        <input onChange={handleChange} value={inputValues.subMonths} type="text" name="subMonths" placeholder="subMonths"/>
+        <textarea onChange={handleChange} value={inputValues.description} type="text" name="description" placeholder="description"/>
+
+        <button>
+          Save new SUBSCRIBER!
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default Form
