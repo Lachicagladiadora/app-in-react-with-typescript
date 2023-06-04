7bcf0f8d43be9e14e6c8913ebd8a6e5b3902e945
Lachicagradiadora <200379@unsaac.edu.pe>
2023-06-03 22:08:53 -0500

    render list component and create file of form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,70 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import List from './components/List'
+import Form from './components/Forms'
 import './App.css'
+// import List from './components/List'
+
+interface AppState {
+  subscriber: Array<Sub>
+  // subscriber: <Sub[]>
+  newSubsNumber: number
+}
+
+const INITIAL_STATE = [
+  {
+    nick: 'monica', 
+    subMonths: 3,
+    avatar: 'https://i.pravatar.cc/150?u=monica',
+    description: 'Monica is a moderator sometimes'
+  },
+  {
+    nick: 'adolfo', 
+    subMonths: 3,
+    avatar: 'https://i.pravatar.cc/150?u=adolfo',
+  }
+]
+
+interface  Sub { 
+  nick: string
+  avatar: string
+  subMonths: number
+  description?: string
+}
 
 function App() {
-  const [number, setNumber] = useState<number | string>(5)
+  // const [subscriber, setSubscriber] = useState<Sub[]>([])  // first form
+  const [subscriber, setSubscriber] = useState<AppState["subscriber"]>([])
+  const [newSubsNumber, setNewSubsNumber] = useState<AppState["newSubsNumber"]>(0)
 
-  const changeNumber = () => {
-    setNumber('2')
-  }
+
+  useEffect(() => {
+    setSubscriber(INITIAL_STATE)
+  }, [])
 
   return (
     <>
-      <h1>App in React with Typescript</h1>
-      <div className="card">
-        {number}
-        <button onClick={changeNumber}>
-          Change number
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <h5 
+        style={{
+          marginBlockStart: '0'
+        }}
+      >
+        App in React with Typescript
+      </h5>
+      
+      <div className="app">
+        <h2 
+          style={{
+            textTransform: 'uppercase'
+          }}
+        >
+          subscribers
+        </h2>
+
+        <List subscriber={subscriber}/>
+
+        <Form/>
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/components/List.tsx
@@ -0,0 +1,55 @@
+// import { JSXElementConstructor } from "react"
+
+interface Props {
+  subscriber: Array<{
+    nick: string
+    avatar: string
+    subMonths: number
+    description?: string
+  }>
+}
+
+export default function List ({subscriber}: Props) {
+  const renderList = (): JSX.Element[] => {
+    return subscriber.map( sub => {
+        return(
+          <li 
+            key={sub.nick}
+          >
+            <img 
+              src={sub.avatar} 
+              alt={`Avatar for ${sub.nick}`} 
+              style={{
+                borderRadius: '200px',
+                height: 'auto',
+                width: '50px',
+              }}
+            />
+            <h4>
+              {sub.nick}
+              <small>
+                {sub.subMonths}
+              </small>
+            </h4>
+            <p>
+              {sub.description?.substring(0, 100)}
+            </p>
+          </li>
+        )
+      })
+  }
+  return(
+    <ul
+          style={{
+            margin:'0',
+            padding: '0',
+            display: 'flex',
+            flexDirection: 'column', 
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+        >
+          {renderList()}
+        </ul>
+  )
+}
